563e0d54a6b75ad236aed322316b09ef9a5c2316
nim3xh <nim3xh@gmail.com>
2023-08-20 05:16:32 +0530

    switch test

---
+++ switch.dart
@@ -0,0 +1,15 @@
+void main(){
+  String name="nimesh";
+
+  switch(name){
+    case "nimesh":
+      print("Hi Nimesh...");
+      break;
+    case "ruwan":
+      print("Hi Ruwan....");
+      break;
+    default:
+      print("Yo...");
+      break;
+  }
+}
